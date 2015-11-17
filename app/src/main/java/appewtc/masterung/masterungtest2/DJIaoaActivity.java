e45e0c502b5a86cfae583b89c6acbe14263d4d71
masterUNG <phrombutr@gmail.com>
2015-11-17 12:23:54 +0700

    Create AoA

---
+++ app/src/main/java/appewtc/masterung/masterungtest2/DJIaoaActivity.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.masterungtest2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DJIaoaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_djiaoa);
+    }
+}
